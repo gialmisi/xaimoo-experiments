880b32d4d11b90d2e85e3125761096d3e20e55f4
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2023-03-27 09:37:34 +0300

    changed project structure.

---
+++ xaimoo/utilities/__init__.py
@@ -0,0 +1,7 @@
+__all__ = [
+    "label_two_bar_truss",
+    "label_vehicle_crash",
+    "label_welded_beam",
+]
+
+from xaimoo.utilities.data import label_two_bar_truss, label_vehicle_crash, label_welded_beam
